5666e3c80f3f029f69291833d4430e00c3772ed3
Patryk Pastuszka <nov11157@gmail.com>
2025-08-29 14:23:01 +0200

    Added example with single modifier affecting multiple resistances.

---
+++ Examples/Entities/ExampleElementalResistanceEntity.cs
@@ -0,0 +1,18 @@
+﻿using Systems.SimpleEntities.Data.Resistances;
+using Systems.SimpleStats.Implementations;
+
+namespace Systems.SimpleEntities.Examples.Entities
+{
+    /// <summary>
+    ///     Entity with all resistances + 50%
+    /// </summary>
+    public sealed class ExampleElementalResistanceEntity : ExampleEntityBase
+    {
+        public override void RefreshModifiersIfNecessary()
+        {
+            statModifiers.Clear();
+            statModifiers.Add(new FlatAddModifier<ResistanceBase>(0.5f));
+            base.RefreshModifiersIfNecessary();
+        }
+    }
+}
